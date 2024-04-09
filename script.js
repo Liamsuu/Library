800f2de0114d9155266e956c7f8244c39224788a
Liam <liamsuu14974@gmail.com>
2024-04-09 16:49:24 +0100

    Fix duplicate bug

--- script.js
+++ script.js
@@ -13,32 +13,31 @@ function addBookToLibrary(bookObject) {
 }
 
 function displayBooks() {
-  for (const object of myLibrary) {
-    const book_wrapper = document.querySelector("#books-wrapper");
-    const book_div = document.createElement("div");
-    book_div.className = "books";
-    book_wrapper.appendChild(book_div);
-    const title = document.createElement("h3");
-    title.className = "book-title";
-    title.textContent = object.title;
-    book_div.appendChild(title);
-    const book_content = document.createElement("div");
-    book_content.className = "book-content"; // append content like author, etc.. inside here.
-    book_div.appendChild(book_content);
-    const author = document.createElement("p");
-    const pages = document.createElement("p");
-    const read = document.createElement("p");
-    author.textContent = `Author: ${object.author}`;
-    pages.textContent = `Pages: ${object.pages}`;
-    if (object.read === true) {
-      read.textContent = `Read: Yes`;
-    } else if (object.read === false) {
-      read.textContent = `Read: No`;
-    }
-    book_content.appendChild(author);
-    book_content.appendChild(pages);
-    book_content.appendChild(read);
+  const object = myLibrary[myLibrary.length - 1];
+  const book_wrapper = document.querySelector("#books-wrapper");
+  const book_div = document.createElement("div");
+  book_div.className = "books";
+  book_wrapper.appendChild(book_div);
+  const title = document.createElement("h3");
+  title.className = "book-title";
+  title.textContent = object.title;
+  book_div.appendChild(title);
+  const book_content = document.createElement("div");
+  book_content.className = "book-content"; // append content like author, etc.. inside here.
+  book_div.appendChild(book_content);
+  const author = document.createElement("p");
+  const pages = document.createElement("p");
+  const read = document.createElement("p");
+  author.textContent = `Author: ${object.author}`;
+  pages.textContent = `Pages: ${object.pages}`;
+  if (object.read === true) {
+    read.textContent = `Read: Yes`;
+  } else if (object.read === false) {
+    read.textContent = `Read: No`;
   }
+  book_content.appendChild(author);
+  book_content.appendChild(pages);
+  book_content.appendChild(read);
 }
 
 // const testerBook = new Book("John Anon", "Test title", 432, true);
@@ -75,3 +74,5 @@ new_book_btn.addEventListener("click", (event) => {
 // then adds whatever book to array and then again display the updated array with the new book
 
 // const book_wrapper = document.querySelector("#books-wrapper");
+
+// TO FIX DUPLICATE BUG, JUST DISPLAY THE RESULT ON THE END. SO JUST FIND THE LENGTH OF ARRAY - 1.
